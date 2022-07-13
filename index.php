133c91dcc080717fc17b8f9910fb0ae001fd391d
Rayanchupranaykumar <107166857+Rayanchupranaykumar@users.noreply.github.com>
2022-07-13 19:18:44 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head><title>Gpa calculator</title>
+ <link href="styles.css" rel="stylesheet" />
+ <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7754624932884765" crossorigin="anonymous"></script>
+</head>
+<body>
+ <center>
+<div class="container justify-content-center">
+    <br>
+    <br>
+    <h2 style="color: red;">R18 CSE GPA calculator</h2>
+    <br>
+    <br>
+    <table class="table">
+        <tr><td class="text-center">
+    <form method="post" action="2-1.html">
+    <button class="btn btn-primary md-2">2-1</button></form></td><td class="text-center">
+    <form method="post" action="2-2.html"><button class="btn btn-primary">2-2</button></form></td></div></tr>
+    <tr>
+    <td class="text-center">
+    <form method="post" action="3-1.html">
+    <button class="btn btn-primary md-2">3-1</button></form></td><td class="text-center">
+    <form method="post" action="3-2.html"><button class="btn btn-primary">3-2</button></form></td></div>
+</tr>
+</table>
+</center>
+<!-- Footer-->
+<footer class="footer py-4 bg-light">
+    <div class="container">
+        <div class="row align-items-center"><div class="navbar navbar-fixed-bottom">
+            <div class="col-lg-4 text-lg-start">Created by &copy; Pranaykumar rayanchu</div>
+            <div class="col-lg-4 my-3 my-lg-0">
+                <a class="btn btn-dark btn-social mx-2" href="https://www.instagram.com/pranay_kumarrr/"><i class="fab fa-instagram"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="https://github.com/Rayanchupranaykumar"><i class="fab fa-github"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="https://www.linkedin.com/in/pranay-kumar-b9a403209"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+            <div class="col-lg-4 text-lg-end">
+                <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
+                <a class="link-dark text-decoration-none" href="#!">Terms of Use</a>
+            </div>
+        </div></div>
+    </div>
+    <br>
+</footer>      
+<!-- Bootstrap core JS--><script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.js"></script>
+<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+</body>
+</html>
